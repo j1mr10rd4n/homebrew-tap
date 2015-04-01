19b7d3053a56a3dd9b4acd17b751c2de4c6e92d4
Jim Riordan <jim@j1mr10rd4n.info>
2015-04-01 14:59:57 +0900

    meteor-1.1 released

--- meteor.rb
+++ meteor.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Meteor < Formula
   homepage "https://www.meteor.com"
-  url "https://d3sqy0vbqsdhku.cloudfront.net/packages-bootstrap/1.0/meteor-bootstrap-os.osx.x86_64.tar.gz", :using => :nounzip
-  sha1 "795908ab21b26e57d09e0c289b6279a125ef6e2f"
-  version "1.0"
+  url "https://d3sqy0vbqsdhku.cloudfront.net/packages-bootstrap/1.1/meteor-bootstrap-os.osx.x86_64.tar.gz", :using => :nounzip
+  sha1 "3c24ba1e59e357d008c9679966fed4b115ef778a"
+  version "1.1"
 
   def install
     system "tar xvfz meteor-bootstrap-os.osx.x86_64.tar.gz"
